6a23b641ebf92941a0dbc0e97dd6c69c1c6ae21e
Matheus Soares <mathsoares1@gmail.com>
2019-11-28 14:35:03 -0400

    Adicionado laço que espera por novas mensagens

--- WhatsAPI/whats.py
+++ WhatsAPI/whats.py
@@ -1,20 +1,46 @@
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-driver.get("https://web.whatsapp.com/")
+def waitForNewMessages():
+  calls = webPage.find_elements_by_class_name(NOTIFICATIONFLAG)
 
-name = input("Digitar nome de usuario ou grupo : ")
-msg = input("Digite sua mensagem : ")
-count = int(input("Digite o count : "))
+  while(len(calls) == 0):
+    calls = webPage.find_elements_by_class_name(NOTIFICATIONFLAG)
+
+  return calls
+
+####
+##  Configuration
+MSGBOX = "_3u328"
+SENDBUTTON = "_3M-N-"
+NOTIFICATIONFLAG = "P6z4j"
+##
+####
+
+webPage = webdriver.Firefox()
+webPage.get("https://web.whatsapp.com/")
 
 input("Digite qualuqer coisa depois de scanear o codigo QR")
 
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
+calls = waitForNewMessages()
+print("Atendendo...")
 
-msgBox = driver.find_element_by_class_name("_3u328")
 
-for i in range(count):
-  msgBox.send_keys(msg)
-  button = driver.find_element_by_class_name("_3M-N-")
-  button.click()
+
+
+
+
+# name = input("Digitar nome de usuario ou grupo : ")
+# msg = input("Digite sua mensagem : ")
+# count = int(input("Digite o count : "))
+
+# input("Digite qualuqer coisa depois de scanear o codigo QR")
+
+# user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+# user.click()
+
+# msgBox = driver.find_element_by_class_name("_3u328")
+
+# for i in range(count):
+#   msgBox.send_keys(msg)
+#   button = driver.find_element_by_class_name("_3M-N-")
+#   button.click()
