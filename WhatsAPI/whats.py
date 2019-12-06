2297dd2e230d803fc82841ad6ecd6a3b167d45e8
Matheus Soares <mathsoares1@gmail.com>
2019-12-06 17:18:21 -0400

    Respondendo mensagens individualmente, tratando algumas excessoes

--- WhatsAPI/whats.py
+++ WhatsAPI/whats.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common import exceptions
 import pdb
 
 ####
@@ -17,31 +18,42 @@ class Whats():
     self.page.get("https://web.whatsapp.com/")
     input("Digite qualquer coisa depois de scanear o codigo QR")
 
-  def waitForNewMessages(self):
+  def getLastText(self):
+    messages = self.page.find_elements_by_class_name(MESSAGEIN)
+    lastMessage = messages.pop()
+    textSpan = lastMessage.find_element_by_class_name(MESSAGESPAN)
+
+    try:
+      text = textSpan.find_element_by_tag_name("span").text
+    except exceptions.NoSuchElementException:
+      text = ""
+
+    return text
+
+  def waitForNewMessage(self):
+    # pdb.set_trace()
     calls = self.page.find_elements_by_class_name(NOTIFICATIONFLAG)
 
     while(len(calls) == 0):
       calls = self.page.find_elements_by_class_name(NOTIFICATIONFLAG)
 
-    return calls
+    call = calls.pop()
 
-  def getLastText(self):
-    messages = self.page.find_elements_by_class_name(MESSAGEIN)
-    lastMessage = messages.pop()
-    textSpan = lastMessage.find_element_by_class_name(MESSAGESPAN)
-    text = textSpan.find_element_by_tag_name("span").text
-
-    return text
-
-  def run(self):
-    calls = self.waitForNewMessages()
-    for call in calls:
+    try:
       call.click()
-      text = self.getLastText()
-      return (call,text)
+    except exceptions.StaleElementReferenceException:
+      call = None
+
+    text = self.getLastText()
+    
+    return (call,text)
 
   def answer(self, call, answer):
-    call.click()
+    try:
+      call.click()
+    except exceptions.StaleElementReferenceException:
+      pass
+
     msgBox = self.page.find_element_by_class_name(MSGBOX)
     msgBox.send_keys(answer)
     button = self.page.find_element_by_class_name(SENDBUTTON)
--- bot.py
+++ bot.py
@@ -1,20 +1,29 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-import classificadores
 from WhatsAPI.whats import Whats
+import classificadores
+import time
+import pdb
+
 
 if __name__ == "__main__":
   vectorizer = TfidfVectorizer(sublinear_tf=True,
     max_df=0.5, strip_accents="unicode")
   
   chosenClassifier = classificadores.findBestModel()
-  print("Best score was %.2f from %s" 
+  print("Best score was %.2f from %s\n" 
     % (chosenClassifier.looFinalScore[0]["accuracy"], chosenClassifier.name))
 
   whats = Whats()
 
   while(True):
-    call, text = whats.run()
-    vec = classificadores.vectorizer
-    prediction = chosenClassifier.predict(vec.transform([text]))
-    whats.answer(call, prediction)
+    # pdb.set_trace()
+    call, text = whats.waitForNewMessage()
+
+    if (text != "" and call != None):
+      vec = classificadores.vectorizer
+      prediction = chosenClassifier.predict(vec.transform([text]))[0]
+      whats.answer(call, prediction)
+      time.sleep(0.75)
+
+    call = None
+    text = ""
